5c1f90a30b4ac0836cf4bf4cccc934d4a53f8786
Roshaun Thompson <roshaun999@icloud.com>
2023-12-07 03:48:36 -0500

    final commit

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ const words = [
     ['broccoli', 'vegetable'],
     ['spinach', 'vegetable'],
     ['lettuce' , 'vegetable'],
+    ['america' , 'country'],
+    ['japan' , 'country'],
+    ['england' , 'country'],
+    ['china' , 'country'],
 ];
 
 let currentWord;
@@ -16,20 +20,22 @@ let incorrectGuesses = 0;
 let maxGuesses = 6;
 
 function newGame() {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    currentWord = words[randomIndex][0];
-    wordCategory = words[randomIndex][1];
+const randomIndex = Math.floor(Math.random() * words.length);
+currentWord = words[randomIndex][0];
+wordCategory = words[randomIndex][1];
 
-    guessedLetters = [];
-    incorrectGuesses = 0;
+guessedLetters = [];
+incorrectGuesses = 0;
 
-    updateDisplay();
+updateDisplay();
 }
+
 
 function updateDisplay() {
     const wordDisplay = document.getElementById('word');
     const categoryDisplay = document.getElementById('category');
     const statusDisplay = document.getElementById('status');
+    const incorrectGuessesDisplay = document.getElementById('incorrectGuesses');
 
     wordDisplay.innerHTML = '';
     categoryDisplay.innerHTML = wordCategory;
@@ -43,19 +49,20 @@ function updateDisplay() {
     });
 
     if (incorrectGuesses === maxGuesses) {
-        statusDisplay.innerHTML = 'You lose!';
+        statusDisplay.innerHTML = 'Try Again?';
     } else if (wordDisplay.innerHTML === currentWord.split('').join(' ') + ' ') {
-        statusDisplay.innerHTML = 'You win!';
+        statusDisplay.innerHTML = 'Your smart!';
     } else {
         statusDisplay.innerHTML = '';
     }
+
+    incorrectGuessesDisplay.innerHTML = 'Incorrect guesses: ' + incorrectGuesses;
 
     // Disable the restart button after the maximum number of guesses
     if (incorrectGuesses === maxGuesses) {
         document.getElementById('restartButton').disabled = true;
     }
 }
-
 function makeGuess() {
     const letter = document.getElementById('letterInput').value.toLowerCase();
     document.getElementById('letterInput').value = '';
