f60f7f42e2915446bc6c6c97875522ff8efda10a
Roshaun Thompson <roshaun999@icloud.com>
2023-12-07 09:59:31 -0500

    final commit

--- js/app.js
+++ js/app.js
@@ -20,16 +20,18 @@ let incorrectGuesses = 0;
 let maxGuesses = 6;
 
 function newGame() {
-const randomIndex = Math.floor(Math.random() * words.length);
-currentWord = words[randomIndex][0];
-wordCategory = words[randomIndex][1];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    currentWord = words[randomIndex][0];
+    wordCategory = words[randomIndex][1];
 
-guessedLetters = [];
-incorrectGuesses = 0;
+    guessedLetters = [];
+    incorrectGuesses = 0;
 
-updateDisplay();
+    updateDisplay();
+
+    // Enable the restart button after each new game
+    document.getElementById('restartButton').disabled = false;
 }
-
 
 function updateDisplay() {
     const wordDisplay = document.getElementById('word');
@@ -39,7 +41,6 @@ function updateDisplay() {
 
     wordDisplay.innerHTML = '';
     categoryDisplay.innerHTML = wordCategory;
-
     currentWord.split('').forEach(letter => {
         if (guessedLetters.includes(letter)) {
             wordDisplay.innerHTML += letter + ' ';
@@ -49,9 +50,9 @@ function updateDisplay() {
     });
 
     if (incorrectGuesses === maxGuesses) {
-        statusDisplay.innerHTML = 'Try Again?';
+        statusDisplay.innerHTML = 'You lose! Try Again?';
     } else if (wordDisplay.innerHTML === currentWord.split('').join(' ') + ' ') {
-        statusDisplay.innerHTML = 'Your smart!';
+        statusDisplay.innerHTML = 'You win! Your smart!';
     } else {
         statusDisplay.innerHTML = '';
     }
@@ -63,21 +64,18 @@ function updateDisplay() {
         document.getElementById('restartButton').disabled = true;
     }
 }
+
 function makeGuess() {
     const letter = document.getElementById('letterInput').value.toLowerCase();
     document.getElementById('letterInput').value = '';
-
     if (!guessedLetters.includes(letter)) {
         guessedLetters.push(letter);
-
         if (!currentWord.includes(letter)) {
             incorrectGuesses++;
         }
     }
-
     updateDisplay();
 }
-
 document.getElementById('letterButton').addEventListener('click', makeGuess);
 document.getElementById('restartButton').addEventListener('click', newGame);
 newGame();
